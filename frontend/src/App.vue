936297fdfe36d3fe0c8ae69e8321a823eb3fdd12
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-03-22 00:04:16 +0300

    refactore divs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,407 @@
-<script>
-    
-</script>
-
 <template>
-    <div>
-        <router-view></router-view>
+    <div class="page">
+        <header>
+            <div class="logo">
+                <img src="./assets/logo.png" width="70px">
+            </div>
+            <div class="title">
+                <h1>Ярославский промышленно- экономический колледж <br/></h1>
+                <h2>имени Н. П. Пастухова</h2>
+            </div>
+
+            <div class="refs-header">
+                <div class="refs-header2">
+                    <label>Кабинеты</label>
+                    <label>Преподавателю</label>
+                    <label>Расписание</label>
+                    <label>Замены</label>
+
+                    <div class="menu-bttn" @click="toggleMenu">
+                        <div class="burger">
+                            <span class="line" v-bind:class="{ open: isOpen }"></span>
+                            <span class="line" v-bind:class="{ open: isOpen }"></span>
+                            <span class="line" v-bind:class="{ open: isOpen }"></span>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div> 
+        </header>
+
+        <main>
+            <div class="menu">
+                <div class="menu-ref">
+                    <div class="icon">
+                        <img src="./assets/abiturientam.png" width="20px"/>
+                    </div>
+                    <h2>Абитуриентам</h2>
+                </div>
+                <div class="menu-ref">
+                    <div class="icon">
+                        <img src="./assets/teachers.png" width="20px"/>
+                    </div>
+                    <h2>Преподавателям</h2>
+                </div>
+                <div class="menu-ref">
+                    <div class="icon">
+                        <img src="./assets/students.png" width="20px"/>
+                    </div>
+                    <h2>Студентам</h2>
+                </div>
+                <div class="menu-ref">
+                    <div class="icon">
+                        <img src="./assets/news.png" width="20px"/>
+                    </div>
+                    <h2>Новости</h2>
+                </div>
+                <div class="menu-ref">
+                    <div class="icon">
+                        <img src="./assets/college.png" width="20px"/>
+                    </div>
+                    <h2>О колледже</h2>
+                </div>
+                <div class="menu-ref" style="padding-bottom: 4px;">
+                    <div class="icon">
+                        <img src="./assets/tech.png" width="20px"/>
+                    </div>
+                    <h2>Техническое отделение</h2>
+                </div>
+                <div class="menu-ref" style="padding-bottom: 10px;">
+                    <div class="icon">
+                        <img src="./assets/himtech.png" width="20px"/>
+                    </div>
+                    <h2>Химико-технологическое отделение</h2>
+                </div>
+                <div class="menu-ref" style="padding-bottom: 4px;">
+                    <div class="icon">
+                        <img src="./assets/finprav.png" width="20px"/>
+                    </div>
+                    <h2>Финансово-правовое отделение</h2>
+                </div>
+                <div class="menu-ref" style="padding-bottom: 4px;">
+                    <div class="icon">
+                        <img src="./assets/zaoch.png" width="20px"/>
+                    </div>
+                    <h2>Заочное отделение</h2>
+                </div>
+                <div class="menu-ref" style="padding-bottom: 4px;">
+                    <div class="icon">
+                        <img src="./assets/dopobr.png" width="20px"/>
+                    </div>
+                    <h2>Дополнительное образование</h2>
+                </div>
+                <div class="menu-ref">
+                    <div class="icon">
+                        <img src="./assets/professionalitet.png" width="20px"/>
+                    </div>
+                    <h2>Профессионалитет</h2>
+                </div>
+                <div class="menu-ref">
+                    <div class="icon">
+                        <img src="./assets/workshops.png" width="20px"/>
+                    </div>
+                    <h2>Мастерские</h2>
+                </div>
+            </div>
+
+            <div class="info">
+                <router-view></router-view>
+            </div>
+
+            <div class="body">
+                
+            </div>
+
+        </main>
+
+        <footer></footer>
     </div>
 </template>
 
+<script>
+    export default {
+        data() {
+            return {
+                isOpen: false,
+            }
+        },
+
+        methods: {
+            toggleMenu() {
+                this.isOpen = !this.isOpen;
+            }
+        }
+    }
+</script>
+
 <style scoped>
+    .page {
+        position: absolute;
+        left: 0;
+        top: 0;
+
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+
+        width: 100%;
+        height: 2104px;
+        min-width: 1000px;
+    }
+    header {
+        position: relative;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 80px;
+        margin-bottom: 30px;
+        display: flex;
+        flex-direction: row;
+
+        box-sizing: border-box;
+        border: 0.5px solid rgb(0, 139, 135);
+        background: linear-gradient(90.00deg, rgb(0, 211, 213) 0.001%,
+            rgb(31, 139, 132) 24.1%,rgb(43, 152, 138) 46.6%,
+            rgb(54, 152, 131) 72.1%,rgb(25, 124, 102) 99.999%);
+    }
+
+    .logo {
+        position: relative;
+        margin-left: 16%;
+        top: 5px;
+        cursor: pointer;
+    }
+
+    .title {
+        margin-left: 10px;
+        height: 90%;
+        cursor: pointer;
+    }
+
+    h1 {
+        width: 280px;
+        height: 70%;
+        margin-top: 3px;
+        font-size: 17px;
+        margin-bottom: 1px;
+
+        /* Ярославский промышленно-экономический колледж имени Н. П. Пастухова */
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        position: relative;
+        font-family: 'Montserrat', sans-serif;  
+        font-weight: 700;
+        color: rgb(255, 255, 255);  
+        
+    }
+
+    .title h2 {
+        margin-top: 0px;
+        font-size: 12px;
+        font-family: 'Montserrat', sans-serif;  
+        font-style: normal;
+        font-weight: 300;
+        color: rgb(255, 255, 255);  
+    }
+
+    .refs-header {
+        position: relative;
+        width: 41.5%;
+        align-items: center;
+        margin-left: 40px;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .refs-header2 {
+        box-sizing: border-box;
+        display: flex;
+        flex-wrap: wrap;
+
+        position: absolute;
+        right: 0px;
+        align-items: center;
+    }
+
+    .refs-header2 label, .menu-bttn {
+        margin-left: 28px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        color: rgb(255, 255, 255);
+        font-family: 'Montserrat', sans-serif;  
+        font-size: 13px;
+        font-weight: 500;
+        line-height: 20px;
+        letter-spacing: 0%;
+        text-align: center;
+        cursor: pointer;
+        height: 20px;
+    }
+
+    .refs-header2 label:hover {
+        color: rgb(210, 210, 210);
+    }
+
+    .menu-bttn {
+        height: 40px;
+        width: 40px;
+
+        border-radius: 5px;
+        box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.25);
+        background: rgb(46, 161, 135);
+    }
+
+    .menu-bttn:hover {
+        background: rgb(40, 142, 118);
+    }
+
+    .menu-bttn:hover .line {
+        background: rgb(210, 210, 210);
+    }
+
+    .burger {
+        position: relative;
+        left: 4px;
+        height: 30px;
+        align-content: center;
+    }
+
+    .line {
+      display: block;
+      width: 21px;
+      height: 1.5px;
+      margin: 6px;
+      background-color: white;
+      transition: all 0.3s ease;
+   }
+
+    .line.open:nth-child(1) {
+        transform: rotate(45deg) translate(5px, 5.5px);
+    }
+
+    .line.open:nth-child(2) {
+        opacity: 0;
+    }
+
+    .line.open:nth-child(3) {
+        transform: rotate(-45deg) translate(5px, -5px);
+    }
+
+    main {
+        box-sizing: border-box;
+        display: flex;
+        flex-wrap: wrap;
+        align-tracks: left;
+        margin-bottom: 30px;
+
+        display: grid;
+        grid-template-columns: 25% 75%;
+        grid-template-rows: 40% 57.69%;
+        grid-template-areas: 
+            "menu info"
+            "body body";
+        column-gap: 30px;
+
+        position: relative;
+        left: 16%;
+        top: 0;
+        width: 67.7%;
+        min-width: 800px;
+        height: 63%;
+    }
+
+    .menu {
+        position: relative;
+        grid-area: menu;
+        width: 100%;
+        height: 530px;
+        padding: 5px;
+
+        box-sizing: border-box;
+        border: 2px solid rgba(136, 136, 136, 0.17);
+        border-radius: 20px;
+        box-shadow: 1px 1px 2px 0px rgba(0, 0, 0, 0.25);
+        background: rgb(255, 255, 255);
+    }
+
+    .menu-ref {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        height: 34px;
+        cursor: pointer;
+    }
+
+    .menu-ref:hover h2 {
+        color: rgb(44, 154, 128);
+    }
+
+    .icon {
+        margin-top: 4px;
+        margin-left: 5px;
+    }
+
+    .menu h2 {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        color: rgb(0, 0, 0);
+        font-family: 'Montserrat', sans-serif;  
+        font-size: 13px;
+        font-weight: 400;
+        line-height: 20px;
+        letter-spacing: 0%;
+        text-align: left;
+        margin-left: 4px;
+        line-height: 18px;
+    }
+
+    .info {
+        position: relative;
+        grid-area: info;
+        width: 96.1%;
+        height: 530px;
+
+        display: grid;
+        grid-template-columns: 68% 28%;
+        grid-template-rows: 32% 62.3%;
+        grid-template-areas: 
+            "swap aside"
+            "ad ad";
+        column-gap: 30px;
+
+    }
+
+    .body {
+        position: relative;
+        grid-area: body;
+        width: 97.2%;
+        height: 100%;
+        margin-top: 30px;
+
+        box-sizing: border-box;
+        border: 2px solid rgba(136, 136, 136, 0.17);
+        border-radius: 20px;
+        box-shadow: 1px 1px 2px 0px rgba(0, 0, 0, 0.25);
+        background: rgb(255, 255, 255);
+    }
+
+    footer {
+        position: relative;
+        display: flex;
+        width: 68%;
+        height: 149px;
+
+        left: 16%;
+
+        box-sizing: border-box;
+        border: 2px solid rgba(136, 136, 136, 0.17);
+        border-radius: 20px;
+        box-shadow: 1px 1px 2px 0px rgba(0, 0, 0, 0.25);
+        background: rgb(255, 255, 255);
+    }
 
 </style>
--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,390 +1,56 @@
 <template>
-    <div class="page">
-        <header>
-            <div class="logo">
-                <img src="../assets/logo.png" width="70px">
-            </div>
-            <div class="title">
-                <h1>Ярославский промышленно- экономический колледж <br/></h1>
-                <h2>имени Н. П. Пастухова</h2>
-            </div>
 
-            <div class="refs-header">
-                <div class="refs-header2">
-                    <label>Кабинеты</label>
-                    <label>Преподавателю</label>
-                    <label>Расписание</label>
-                    <label>Замены</label>
+        <div class="swap">
+            <img src="../assets/prof-label.png" width="90%">
+        </div>
 
-                    <div class="menu-bttn" @click="toggleMenu">
-                        <div class="burger">
-                            <span class="line" v-bind:class="{ open: isOpen }"></span>
-                            <span class="line" v-bind:class="{ open: isOpen }"></span>
-                            <span class="line" v-bind:class="{ open: isOpen }"></span>
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
-            
-        </header>
+        <div class="aside">
+            <img src="../assets/r-farm.png" width="90%">
+        </div>
 
-        <main>
-            <div class="menu">
-                <div class="menu-ref">
-                    <div class="icon">
-                        <img src="../assets/abiturientam.png" width="20px"/>
-                    </div>
-                    <h2>Абитуриентам</h2>
-                </div>
-                <div class="menu-ref">
-                    <div class="icon">
-                        <img src="../assets/teachers.png" width="20px"/>
-                    </div>
-                    <h2>Преподавателям</h2>
-                </div>
-                <div class="menu-ref">
-                    <div class="icon">
-                        <img src="../assets/students.png" width="20px"/>
-                    </div>
-                    <h2>Студентам</h2>
-                </div>
-                <div class="menu-ref">
-                    <div class="icon">
-                        <img src="../assets/news.png" width="20px"/>
-                    </div>
-                    <h2>Новости</h2>
-                </div>
-                <div class="menu-ref">
-                    <div class="icon">
-                        <img src="../assets/college.png" width="20px"/>
-                    </div>
-                    <h2>О колледже</h2>
-                </div>
-                <div class="menu-ref" style="padding-bottom: 4px;">
-                    <div class="icon">
-                        <img src="../assets/tech.png" width="20px"/>
-                    </div>
-                    <h2>Техническое отделение</h2>
-                </div>
-                <div class="menu-ref" style="padding-bottom: 10px;">
-                    <div class="icon">
-                        <img src="../assets/himtech.png" width="20px"/>
-                    </div>
-                    <h2>Химико-технологическое отделение</h2>
-                </div>
-                <div class="menu-ref" style="padding-bottom: 4px;">
-                    <div class="icon">
-                        <img src="../assets/finprav.png" width="20px"/>
-                    </div>
-                    <h2>Финансово-правовое отделение</h2>
-                </div>
-                <div class="menu-ref" style="padding-bottom: 4px;">
-                    <div class="icon">
-                        <img src="../assets/zaoch.png" width="20px"/>
-                    </div>
-                    <h2>Заочное отделение</h2>
-                </div>
-                <div class="menu-ref" style="padding-bottom: 4px;">
-                    <div class="icon">
-                        <img src="../assets/dopobr.png" width="20px"/>
-                    </div>
-                    <h2>Дополнительное образование</h2>
-                </div>
-                <div class="menu-ref">
-                    <div class="icon">
-                        <img src="../assets/professionalitet.png" width="20px"/>
-                    </div>
-                    <h2>Профессионалитет</h2>
-                </div>
-                <div class="menu-ref">
-                    <div class="icon">
-                        <img src="../assets/workshops.png" width="20px"/>
-                    </div>
-                    <h2>Мастерские</h2>
-                </div>
+        <div class="ad">
+            <div class="ad-text">
+                12 апреля 2025 года состоится Единый день открытых дверей Федерального проекта
+                <a href="https://ypec.ru/professionalitet/" target="_blank">«Профессионалитет»</a><br/>
+                Приглашаем вас посетить мероприятие по адресу:
+                г. Ярославль, улица Гагарина, дом 8 <br/>
+                Начало мероприятия — 9.00 ч. <br/>
+                <a href="https://ypec.ru/2025/03/%D0%B5%D0%B4%D0%B8%D0%BD%D1%8B%D0%B9-%D0%B4%D0%B5%D0%BD%D1%8C-%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D1%8B%D1%85-%D0%B4%D0%B2%D0%B5%D1%80%D0%B5%D0%B9-2/#more-11492" target="_blank">
+                    Программа проведения</a>
             </div>
 
-            <div class="swap">
-                <img src="../assets/prof-label.png" width="90%">
-            </div>
+            <img src="../assets/ad1.png" width="50%" style="margin: 20px;">
+        </div>
 
-            <div class="aside">
-                <img src="../assets/r-farm.png" width="90%">
-            </div>
-
-            <div class="ad">
-                <div class="ad-text">
-                    12 апреля 2025 года состоится Единый день открытых дверей Федерального проекта
-                    <a href="https://ypec.ru/professionalitet/" target="_blank">«Профессионалитет»</a><br/>
-                    Приглашаем вас посетить мероприятие по адресу:
-                    г. Ярославль, улица Гагарина, дом 8 <br/>
-                    Начало мероприятия — 9.00 ч. <br/>
-                    <a href="https://ypec.ru/2025/03/%D0%B5%D0%B4%D0%B8%D0%BD%D1%8B%D0%B9-%D0%B4%D0%B5%D0%BD%D1%8C-%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D1%8B%D1%85-%D0%B4%D0%B2%D0%B5%D1%80%D0%B5%D0%B9-2/#more-11492" target="_blank">
-                        Программа проведения</a>
-                </div>
-
-                <img src="../assets/ad1.png" width="50%">
-            </div>
-            <div class="body"></div>
-        </main>
-
-        <footer></footer>
-    </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                isOpen: false,
-            }
-        },
-
-        methods: {
-            toggleMenu() {
-                this.isOpen = !this.isOpen;
-            }
-        }
-    }
 
 </script>
 
 <style scoped>
-
     .page {
-        position: absolute;
-        left: 0;
-        top: 0;
-
-        box-sizing: border-box;
-        display: flex;
-        flex-direction: column;
-
-        width: 100%;
-        height: 2104px;
-        min-width: 1000px;
-    }
-    header {
-        position: relative;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 80px;
-        margin-bottom: 30px;
-        display: flex;
-        flex-direction: row;
-
-        box-sizing: border-box;
-        border: 0.5px solid rgb(0, 139, 135);
-        background: linear-gradient(90.00deg, rgb(0, 211, 213) 0.001%,
-            rgb(31, 139, 132) 24.1%,rgb(43, 152, 138) 46.6%,
-            rgb(54, 152, 131) 72.1%,rgb(25, 124, 102) 99.999%);
-    }
-
-    .logo {
-        position: relative;
-        margin-left: 16%;
-        top: 5px;
-        cursor: pointer;
-    }
-
-    .title {
-        margin-left: 10px;
-        height: 90%;
-        cursor: pointer;
-    }
-
-    h1 {
-        width: 280px;
-        height: 70%;
-        margin-top: 3px;
-        font-size: 17px;
-        margin-bottom: 1px;
-
-        /* Ярославский промышленно-экономический колледж имени Н. П. Пастухова */
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        position: relative;
-        font-family: 'Montserrat', sans-serif;  
-        font-weight: 700;
-        color: rgb(255, 255, 255);  
-        
-    }
-
-    .title h2 {
-        margin-top: 0px;
-        font-size: 12px;
-        font-family: 'Montserrat', sans-serif;  
-        font-style: normal;
-        font-weight: 300;
-        color: rgb(255, 255, 255);  
-    }
-
-    .refs-header {
-        position: relative;
-        width: 41.5%;
-        align-items: center;
-        margin-left: 40px;
-        display: flex;
-        flex-direction: row;
-    }
-
-    .refs-header2 {
-        box-sizing: border-box;
-        display: flex;
-        flex-wrap: wrap;
-
-        position: absolute;
-        right: 0px;
-        align-items: center;
-    }
-
-    .refs-header2 label, .menu-bttn {
-        margin-left: 28px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-
-        color: rgb(255, 255, 255);
-        font-family: 'Montserrat', sans-serif;  
-        font-size: 13px;
-        font-weight: 500;
-        line-height: 20px;
-        letter-spacing: 0%;
-        text-align: center;
-        cursor: pointer;
-        height: 20px;
-    }
-
-    .refs-header2 label:hover {
-        color: rgb(210, 210, 210);
-    }
-
-    .menu-bttn {
-        height: 40px;
-        width: 40px;
-
-        border-radius: 5px;
-        box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.25);
-        background: rgb(46, 161, 135);
-    }
-
-    .menu-bttn:hover {
-        background: rgb(40, 142, 118);
-    }
-
-    .menu-bttn:hover .line {
-        background: rgb(210, 210, 210);
-    }
-
-    .burger {
-        position: relative;
-        left: 4px;
-        height: 30px;
-        align-content: center;
-    }
-
-    .line {
-      display: block;
-      width: 21px;
-      height: 1.5px;
-      margin: 6px;
-      background-color: white;
-      transition: all 0.3s ease;
-   }
-
-    .line.open:nth-child(1) {
-        transform: rotate(45deg) translate(5px, 5.5px);
-    }
-
-    .line.open:nth-child(2) {
-        opacity: 0;
-    }
-
-    .line.open:nth-child(3) {
-        transform: rotate(-45deg) translate(5px, -5px);
-    }
-
-    main {
-        box-sizing: border-box;
-        display: flex;
-        flex-wrap: wrap;
-        align-tracks: left;
-        margin-bottom: 30px;
+        height: 100%;
 
         display: grid;
-        grid-template-columns: 23% 50% 21%;
-        grid-template-rows: 10% 21.9% 64.05%;
+        grid-template-columns: 50% 20%;
+        grid-template-rows: 28% 60.7%;
         grid-template-areas: 
-            "menu swap aside"
-            "menu ad ad"
-            "body body body";
+            "swap aside"
+            "ad ad";
         column-gap: 30px;
         row-gap: 30px;
 
-        position: relative;
-        left: 16%;
-        top: 0;
-        width: 68%;
-        min-width: 800px;
-        height: 70%;
-    }
-
-    .menu {
-        position: relative;
-        grid-area: menu;
-        width: 100%;
-        height: 530px;
-        margin-right: 3%;
-        padding: 5px;
-
         box-sizing: border-box;
-        border: 2px solid rgba(136, 136, 136, 0.17);
-        border-radius: 20px;
-        box-shadow: 1px 1px 2px 0px rgba(0, 0, 0, 0.25);
-        background: rgb(255, 255, 255);
-    }
 
-    .menu-ref {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        height: 34px;
-        cursor: pointer;
+        /* background: blue; */
     }
-
-    .menu-ref:hover h2 {
-        color: rgb(44, 154, 128);
-    }
-
-    .icon {
-        margin-top: 4px;
-        margin-left: 5px;
-    }
-
-    .menu h2 {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        color: rgb(0, 0, 0);
-        font-family: 'Montserrat', sans-serif;  
-        font-size: 13px;
-        font-weight: 400;
-        line-height: 20px;
-        letter-spacing: 0%;
-        text-align: left;
-        margin-left: 4px;
-        line-height: 18px;
-    }
-
+    
     .swap {
         position: relative;
         grid-area: swap;
         width: 100%;
-        height: 180px;
-        margin-right: 3%;
+        height: 100%;
         align-content: center;
         text-align: center;
 
@@ -399,7 +65,7 @@
         position: relative;
         grid-area: aside;
         width: 100%;
-        height: 180px;
+        height: 100%;
         align-content: center;
         text-align: center;
 
@@ -449,31 +115,4 @@
         background: rgb(226, 252, 246);
     }
 
-    .body {
-        position: relative;
-        grid-area: body;
-        width: 100%;
-        margin-top: 30px;
-
-        box-sizing: border-box;
-        border: 2px solid rgba(136, 136, 136, 0.17);
-        border-radius: 20px;
-        box-shadow: 1px 1px 2px 0px rgba(0, 0, 0, 0.25);
-        background: rgb(255, 255, 255);
-    }
-
-    footer {
-        position: relative;
-        display: flex;
-        width: 68%;
-        height: 149px;
-
-        left: 16%;
-
-        box-sizing: border-box;
-        border: 2px solid rgba(136, 136, 136, 0.17);
-        border-radius: 20px;
-        box-shadow: 1px 1px 2px 0px rgba(0, 0, 0, 0.25);
-        background: rgb(255, 255, 255);
-    }
 </style>
